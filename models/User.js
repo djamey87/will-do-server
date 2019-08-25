6ce20d288a9c00490b35ef2e10e6d3df7b3217af
djamey <djamey87@gmail.com>
2019-08-25 18:17:43 +0100

    wip security implementation for login

--- models/User.js
+++ models/User.js
@@ -1,12 +1,50 @@
 const mongoose = require('mongoose');
-const {Schema} = mongoose;
+const bcrypt = require('bcrypt');
+const { Schema } = mongoose;
 
-const userSchema = new Schema({
-  	displayName: {
-		type:String,
-		required: true
-	}
+const userSchema = new Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+		},
+		email: {
+			type: String,
+			lowercase: true,
+			unique: true,
+			required: [true, "can't be blank"],
+			match: [/\S+@\S+\.\S+/, 'is invalid'],
+			index: true,
+		},
+		password: {
+			type: String,
+			required: true,
+		},
+	},
+	{ timestamps: true }
+);
 
-},{timestamps:true});
+// generate a hash from user password and change the password to the hash.
+userSchema.pre('save', function(next) {
+	const user = this;
+	const saltFactor = 5;
+
+	if (!user.isModified('password')) return next();
+
+	bcrypt.genSalt(saltFactor, function(err, salt) {
+		if (err) return next(err);
+
+		bcrypt.hash(user.password, salt, function(err, hash) {
+			if (err) return next(err);
+			user.password = hash;
+			next();
+		});
+	});
+});
+
+userSchema.methods.validatePassword = function(password) {
+	var hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
+	return this.hash === hash;
+};
 
 module.exports = mongoose.model('User', userSchema);
