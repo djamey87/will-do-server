e186d4f2ce1d4fafde193b6622e52689de1f0fd5
djamey <djamey87@gmail.com>
2019-09-06 16:31:10 +0100

    amending property names

--- models/Task.js
+++ models/Task.js
@@ -1,13 +1,19 @@
 const mongoose = require('mongoose');
-const {Schema} = mongoose;
+const { Schema } = mongoose;
 
-const taskSchema = new Schema({
-  	title: String,
-  	content: String,
+const taskSchema = new Schema(
+	{
+		title: {
+			type: String,
+			required: true,
+		},
+		content: String,
 
-  	_user: { type: Schema.Types.ObjectId, ref: 'User' }
-},{timestamps:true});
-
-// console.log('taskSchema', taskSchema)
+		author: { type: Schema.Types.ObjectId, ref: 'User' },
+		// can be assigned to multiple users, e.g. a meeting will have multiple attendees
+		assignedUsers: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+	},
+	{ timestamps: true }
+);
 
 module.exports = mongoose.model('Task', taskSchema);
