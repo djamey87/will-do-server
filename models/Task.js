70a5cfafd981c011c9fa118f7e604c03c067996d
djamey <djamey87@gmail.com>
2019-09-08 17:01:18 +0300

    adding status to task model for simple filtering

--- models/Task.js
+++ models/Task.js
@@ -12,6 +12,11 @@ const taskSchema = new Schema(
 		author: { type: Schema.Types.ObjectId, ref: 'User' },
 		// can be assigned to multiple users, e.g. a meeting will have multiple attendees
 		assignedUsers: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+		status: {
+			type: String,
+			enum: ['deleted', 'active'],
+			default: 'active',
+		},
 	},
 	{ timestamps: true }
 );
